1bd302b38d6069ef968c88cfff6435930b5afff0
Polonskiy Igor <barlogfire@ya.ru>
2022-05-12 10:36:58 +0300

    task8-9

---
+++ js-data/task8.js
@@ -0,0 +1,115 @@
+(() => {
+
+    let pictures = [{
+            id: 1,
+            src: 'Images_1/singleChoice/Eng-2021_2_1_3_21.png'
+        },
+        {
+            id: 2,
+            src: 'Images_1/singleChoice/Eng-2021_2_1_3_22.png'
+        },
+        {
+            id: 3,
+            src: 'Images_1/singleChoice/Eng-2021_2_1_3_23.png'
+        }
+    ]
+
+    let task = document.querySelector('.task_8')
+    const interakt_zadanie = task.parentElement;
+    const headCheck = interakt_zadanie.previousElementSibling;
+    const drop = headCheck.querySelector('.drop');
+    const check_your = headCheck.querySelector('.check_your');
+    const result = headCheck.querySelector('.result');
+
+    const winVar = '3'
+
+    let pictureItems = null
+    let finishAnswer = null
+    let startAction = false
+
+    insertAnswers(pictures)
+    task.addEventListener('click', (e) => {
+        if (e.target.classList.contains('task_8_pic')) {
+            pictureItems.forEach(item => {
+                if (finishAnswer) {
+                    finishAnswer.classList.remove('task_8_pic_active')
+                    finishAnswer.classList.remove('task_8_green')
+                    finishAnswer.classList.remove('task_8_red')
+                }
+                item.classList.remove('task_8_pic_active')
+            })
+            e.target.classList.add('task_8_pic_active')
+            finishAnswer = e.target
+            startAction = true;
+            checkButton_classList_changer();
+        }
+    })
+
+
+    function shuffleArr(arr) {
+        return arr.sort(() => Math.random() - 0.5)
+    }
+
+    function insertAnswers(arr) {
+        shuffleArr(pictures).forEach(item => {
+            let picture = document.createElement('div')
+            picture.classList.add('task_8_pic')
+            picture.style.backgroundImage = `url(${item.src})`
+            picture.setAttribute('data-id', item.id)
+            task.append(picture)
+        })
+        pictureItems = task.querySelectorAll('.task_8_pic')
+    }
+
+    drop.addEventListener('click', resetPuzzle);
+
+    function checkButton_classList_changer() {
+        if (check_your.classList.contains('check_your_active') && !startAction) {
+            check_your.classList.remove('check_your_active');
+            check_your.removeEventListener('click', check_task);
+        } else if (!check_your.classList.contains('check_your_active') && startAction) {
+            check_your.removeEventListener('click', check_task);
+            check_your.classList.add('check_your_active');
+            check_your.addEventListener('click', check_task);
+        }
+    }
+
+    function check_task() {
+        if (finishAnswer) {
+            finishAnswer.classList.remove('task_8_pic_active')
+
+            if (finishAnswer.getAttribute('data-id') === winVar) {
+                finishAnswer.classList.add('task_8_green')
+                feedBackChanger('win');
+
+            } else {
+                finishAnswer.classList.add('task_8_red')
+                feedBackChanger('lose');
+
+            }
+        }
+
+
+    }
+
+    function resetPuzzle() {
+        startAction = false;
+        checkButton_classList_changer();
+        feedBackChanger('reset')
+        if (finishAnswer) {
+            finishAnswer.classList.remove('task_8_pic_active')
+            finishAnswer.classList.remove('task_8_green')
+            finishAnswer.classList.remove('task_8_red')
+        }
+        finishAnswer = null
+    }
+
+    function feedBackChanger(state) {
+        if (startAction && state === 'win' || state === 'lose') {
+            result.classList.add(`result_${state}`);
+        } else {
+            result.classList.remove(`result_win`);
+            result.classList.remove(`result_lose`);
+        }
+    }
+})()
---
+++ js-data/task9.js
@@ -0,0 +1,306 @@
+(() => {
+
+    const task = document.querySelector('.task_9')
+    const dropzone = task.querySelector('.task_9_dropzone')
+    const dragzone = task.querySelector('.task_9_dragzone')
+    const interakt_zadanie = task.parentElement;
+    const headCheck = interakt_zadanie.previousElementSibling;
+
+    const drop = headCheck.querySelector('.drop');
+    const check_your = headCheck.querySelector('.check_your');
+    const result = headCheck.querySelector('.result');
+    const wordPazzle_dropWrapper = document.querySelector('.task4_imgWrapper');
+    const wordPazzle_letters = document.querySelector('.task4_answers');
+    let startAction = false;
+    let dropitems
+
+
+    const pictures = [{
+            id: 1,
+            data: 'Images_1/wordsToPic/Eng-2021_2_1_1_25.png'
+        },
+        {
+            id: 2,
+            data: 'Images_1/wordsToPic/Eng-2021_2_1_1_26.png'
+        },
+        {
+            id: 3,
+            data: 'Images_1/wordsToPic/Eng-2021_2_1_1_27.png'
+        },
+        {
+            id: 4,
+            data: 'Images_1/wordsToPic/Eng-2021_2_1_1_28.png'
+        },
+        {
+            id: 5,
+            data: 'Images_1/wordsToPic/Eng-2021_2_1_1_29.png'
+        },
+        {
+            id: 6,
+            data: 'Images_1/wordsToPic/Eng-2021_2_1_1_30.png'
+        },
+        {
+            id: 7,
+            data: 'Images_1/wordsToPic/Eng-2021_2_1_1_31.png'
+        }
+    ]
+
+    let words = [{
+            id: 1,
+            text: 'I am Alex'
+        },
+        {
+            id: 2,
+            text: 'You are Kate'
+        },
+        {
+            id: 3,
+            text: 'He is Nick'
+        },
+        {
+            id: 4,
+            text: 'She is Lara'
+        },
+        {
+            id: 5,
+            text: 'It is Coco'
+        },
+        {
+            id: 7,
+            text: 'We are Alex and Nick'
+        },
+        {
+            id: 6,
+            text: 'They are Kiwi and Coco'
+        }
+    ]
+
+    let drops
+
+    insertPictures(pictures)
+    insertWords(words)
+
+    function shuffleArr(arr) {
+        return arr.sort(() => Math.random() - 0.5)
+    }
+
+    function insertPictures(arr) {
+        shuffleArr(arr).forEach(item => {
+            let picture = document.createElement('div')
+            picture.classList.add('task_9_drop')
+            picture.style.backgroundImage = `url(${item.data})`
+            picture.setAttribute('data-id', item.id)
+            dropzone.append(picture)
+        })
+        dropitems = task.querySelectorAll('.task_9_drop')
+
+    }
+
+    function insertWords(arr) {
+        shuffleArr(arr).forEach(item => {
+            let word = document.createElement('div')
+            word.classList.add('task_9_dragitem')
+            word.innerText = item.text
+            word.setAttribute('data-id', item.id)
+            dragzone.append(word)
+        })
+    }
+
+
+    let draggingItem;
+    let elemBelow;
+
+
+    task.addEventListener('pointerdown', draggingListner);
+
+    function draggingListner(e) {
+        if (e.target.classList.contains('task_9_dragitem')) {
+            mouseDown(e)
+        }
+    }
+
+
+
+    function mouseDown(event) {
+        if (event.button === 2) return;
+        draggingItem = event.target;
+        draggingItem.style.touchAction = 'none'; //ОБЯЗАТЕЛЬНОЕ УСЛОВИЕ(МОЖНО УБРАТЬ И ПРОПИСАТЬ В СТИЛЬ САМОМУ ОБЪЕКТУ) 
+        draggingItem.style.cursor = 'grabbing';
+        let shiftX = event.clientX - draggingItem.getBoundingClientRect().left;
+        let shiftY = event.clientY - draggingItem.getBoundingClientRect().top;
+
+        // ЛИММИТЫ КООРДИНАТ ОГРАНИЧИВАЮЩИЕ ВЫЛЕТ ПЕРЕТАСКИВАЕМОГО ЭЛЕМЕНТА ЗА БЛОК
+        //  (ПО УМОЛЧАНИЮ interact_zadanie - РОДИТЕЛЬ ВАШЕГО БЛОКА)
+        let limits = {
+            top: interakt_zadanie.offsetTop,
+            right: interakt_zadanie.offsetWidth + interakt_zadanie.offsetLeft,
+            bottom: interakt_zadanie.offsetHeight + interakt_zadanie.offsetTop,
+            left: interakt_zadanie.offsetLeft
+        };
+
+        draggingItem.style.position = 'absolute';
+        draggingItem.style.zIndex = 1000;
+        document.body.appendChild(draggingItem);
+
+        moveAt(event.pageX, event.pageY);
+
+        function moveAt(pageX, pageY) {
+            draggingItem.style.left = pageX - shiftX + 'px';
+            draggingItem.style.top = pageY - shiftY + 'px';
+        }
+
+        elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+
+        let clickWithoutMove = true;
+
+        function onMouseMove(event) {
+            let newLocation = {
+                x: limits.left,
+                y: limits.top
+            };
+            if (event.pageX > limits.right) {
+                newLocation.x = limits.right;
+            } else if (event.pageX > limits.left) {
+                newLocation.x = event.pageX;
+            }
+            if (event.pageY > limits.bottom) {
+                newLocation.y = limits.bottom;
+            } else if (event.pageY > limits.top) {
+                newLocation.y = event.pageY;
+            }
+
+            clickWithoutMove = false
+            moveAt(newLocation.x, newLocation.y);
+
+            if (event.path[0] !== draggingItem) {
+                window.addEventListener('pointerup', moveOut);
+            }
+            draggingItem.style.visibility = 'hidden'
+            elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+            draggingItem.style.visibility = 'visible';
+            if (!elemBelow) return;
+
+            // ОБРАБОТКА СОБЫТИЯ НАХОЖДЕНИЯ НАД БЛОКОМ И ВЫЛЕТА ИЗ НЕГО (ПО НЕОБХОДИМИОСТИ)
+
+            // let currentDroppable = null;
+
+            // let droppableBelow = elemBelow.closest('.droppable'); // БЕРЁМ НУЖНЫЙ БЛОК 
+
+            // if (currentDroppable != droppableBelow) {
+            //     if (currentDroppable) { 
+            // ЛОГИКА ОБРАБОТКИ ПРОЦЕССА "ВЫЛЕТА" ИЗ DROPPABLE
+            //         leaveDroppable(currentDroppable);
+            //     }
+            //     currentDroppable = droppableBelow;
+            // ЛОГИКА ОБРАБОТКИ ПРОЦЕССА, КОГДА МЫ "ВЛЕТАЕМ" В ЭЛЕМЕНТ
+            //     if (currentDroppable) {
+            //         enterDroppable(currentDroppable);
+            //     }
+            // }
+        }
+
+        // КОГДА НАД ВЫБРАННЫМ БЛОКОМ
+        function enterDroppable(currentDroppable) {
+            // currentDroppable
+        }
+        // КОДА ВЫЛЕТЕЛИ ИЗ БЛОКА
+        function leaveDroppable(currentDroppable) {
+            // currentDroppable
+        }
+        document.addEventListener('pointermove', onMouseMove);
+
+
+        // КОГДА ВО ВРЕМЯ ПЕРЕТАСКИВАНИЯ КУРСОР ВЫНЕСЛИ ЗА ПРЕДЕЛЫ ОКНА БРАУЗЕРА И ОТПУСТИЛИ ЗАХВАТ ЭЛЕМЕНТА
+        function moveOut(e) {
+            changeStylesAndAppend(dragzone, draggingItem);
+            window.removeEventListener('pointerup', moveOut);
+            document.removeEventListener('pointermove', onMouseMove);
+        }
+
+        // КОГДА КУРСОР В ЗОНЕ ДЛЯ ПЕРЕТАСКИВАНИЙ И ПОЛЬЗОВАТЕЛЬ ОТПУСТИЛ ЗАХВАТ ЭЛЕМЕНТА
+        draggingItem.onpointerup = function() {
+            draggingItem.style.cursor = 'grab';
+            startAction = true;
+            checkButton_classList_changer();
+            if (clickWithoutMove) {
+                changeStylesAndAppend(dragzone, draggingItem);
+            }
+            document.removeEventListener('pointermove', onMouseMove);
+
+            // ЛОГИКА ОБРАБОТКИ ПОПАДАНИЯ НА НУЖНЫЙ БЛОК И НАОБОРОТ
+            if (elemBelow.classList.contains('task_9_drop') && elemBelow.children.length === 0) {
+                changeStylesAndAppend(elemBelow, draggingItem);
+            } else {
+                changeStylesAndAppend(dragzone, draggingItem);
+            }
+        };
+
+        function changeStylesAndAppend(dropPlace, draggingElem) {
+            draggingElem.style.position = 'relative ';
+            draggingElem.style.zIndex = null;
+            draggingElem.style.top = null;
+            draggingElem.style.left = null;
+            dropPlace.appendChild(draggingElem);
+        }
+    };
+
+    drop.addEventListener('click', resetPuzzle);
+
+    function checkButton_classList_changer() {
+
+        if (check_your.classList.contains('check_your_active') && !startAction) {
+            check_your.classList.remove('check_your_active');
+            check_your.removeEventListener('click', checkPuzzle);
+        } else if (!check_your.classList.contains('check_your_active') && startAction) {
+            check_your.removeEventListener('click', checkPuzzle);
+            check_your.classList.add('check_your_active');
+            check_your.addEventListener('click', checkPuzzle);
+        }
+    }
+
+
+    function resetPuzzle() {
+        startAction = false;
+        checkButton_classList_changer();
+        feedBackChanger('reset')
+
+        dropitems.forEach(item => {
+            if (item.children.length) {
+                [...item.children][0].classList.remove('task_9_right');
+                [...item.children][0].classList.remove('task_9_wrong');
+                dragzone.append([...item.children][0])
+
+            }
+        })
+        task.addEventListener('pointerdown', draggingListner);
+    }
+
+    function checkPuzzle() {
+        let winVar = 0
+        dropitems.forEach(item => {
+            if (item.children.length) {
+                if ([...item.children][0].getAttribute('data-id') === item.getAttribute('data-id')) {
+                    winVar++;
+                    [...item.children][0].classList.add('task_9_right')
+                } else [...item.children][0].classList.add('task_9_wrong')
+            }
+        })
+
+        if (winVar === pictures.length) {
+            feedBackChanger('win');
+        } else {
+            feedBackChanger('lose');
+        }
+        task.removeEventListener('pointerdown', draggingListner);
+    }
+
+    function feedBackChanger(state) {
+        if (startAction && state === 'win' || state === 'lose') {
+            result.classList.add(`result_${state}`);
+        } else {
+            result.classList.remove(`result_win`);
+            result.classList.remove(`result_lose`);
+        }
+    }
+
+})()
