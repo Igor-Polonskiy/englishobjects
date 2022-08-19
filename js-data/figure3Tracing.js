0df428edbf3c65e8b535b9117dcf923e269b6fd4
Polonskiy Igor <barlogfire@ya.ru>
2022-08-19 17:55:46 +0300

    square, triangle

---
+++ js-data/figure3Tracing.js
@@ -0,0 +1,215 @@
+(() => {
+
+    /*все вхождения figure2_A... заменить A на нужную букву*/
+    let task = document.querySelector('.figure3_wrapper')
+    const interakt_zadanie = task.parentElement;
+    const headCheck = interakt_zadanie.previousElementSibling;
+    const drop = headCheck.querySelector('.drop');
+    const figure2Picture = task.querySelector('.figure2_picture')
+
+    let c = task.querySelector('.figure2_A_canvas');
+    let cx = c.getContext('2d');
+    let mousedown = false;
+
+    let pixels = null;
+    let figure2pixels = null;
+    /*заменить Aa на нужные буквы*/
+    //let figure2 = '◻'// △ ◯ ▭ ◻ ◇
+    /**/
+    /*при необходимости подобрать нужный шрифт*/
+    //let font = 'Titillium Web'
+    //cx.font = `bold 310px ${font}`;
+    /**/
+    let isfigure2Complete = false
+    /*заменть цвет закраски буквы на нужный в формате rgb*/
+    /*использовать любой кроме 
+    (255,255,255)-цвет буквы и
+    (240, 100, 0)-цвет обводки
+    (значение 255 нельзя применять ни к одому из трех каналов цвета)*/
+    let r = 220
+    let g = 0
+    let b = 0
+    /**/
+    let drowColor = `rgb(${r}, ${g}, ${b})`
+
+    function setupCanvas() {
+        c.height = 400;
+        c.width = 500;
+        cx.lineCap = 'round';
+        // cx.font = `bold 500px ${font}`;
+        //cx.fillStyle = 'rgb(255, 255, 255)';
+        //cx.textBaseline = 'middle';
+
+        drawfigure2(150, 'rgb(230,230,230)');
+        
+        pixels = cx.getImageData(0, 0, c.width, c.height);
+        figure2pixels = getpixelamount(230, 230, 230);
+    }
+
+    function drawfigure2(size, color) {
+        let centerX = c.width / 2;
+        let centerY = c.height / 2;
+        cx.fillStyle =`rgb(${r}, ${g}, ${b+1})`
+        cx.fillRect(centerX - size,centerY-size,2*size,2*size);
+        cx.fillStyle =color
+        cx.fillRect(centerX - size+10,centerY-size+10,2*size-20,2*size-20);
+        cx.fillStyle =`rgb(${r}, ${g}, ${b+1})`
+        cx.fillRect(centerX - size+30,centerY-size+30,2*size-60,2*size-60);
+        cx.fillStyle =`rgb(255, 255, 255)`
+        cx.fillRect(centerX - size+40,centerY-size+40,2*size-80,2*size-80);
+    };
+
+    function strokeStar( r, n, inset, color) {
+        let centerX = c.width / 2;
+        let centerY = c.height / 2;
+        cx.save();
+        cx.beginPath();
+        cx.translate(centerX, centerY);
+        cx.moveTo(0,0-r);
+        for (var i = 0; i < n; i++) {
+            cx.rotate(Math.PI / n);
+            cx.lineTo(0, 0 - (r*inset));
+            cx.rotate(Math.PI / n);
+            cx.lineTo(0, 0 - r);
+        }
+        cx.closePath();
+        cx.fillStyle = color;
+        cx.fill();
+        cx.restore();
+    }
+
+    function drawfigure2Border(figure2) {
+        let centerx = (c.width - cx.measureText(figure2).width) / 2;
+        let centery = c.height / 2;
+        cx.lineWidth = 5;
+        cx.lineCap = 'round'
+        cx.strokeStyle = 'rgb(240, 100, 0)';
+        cx.strokeText(figure2, centerx, centery);
+    };
+
+    function showerror(error) {
+        mousedown = false;
+        alert(error);
+    };
+
+    function paint(x, y) {
+        let colour = getpixelcolour(x, y);
+
+        if (colour.r !== 230 || colour.g !== 230 || colour.b !== 230) {
+
+            mousedown = false;
+        } else {
+            cx.strokeStyle = drowColor;
+
+            cx.lineWidth = 24;
+            cx.beginPath();
+            cx.lineTo(x, y);
+            cx.stroke();
+            cx.beginPath();
+            cx.stroke();
+
+            cx.beginPath();
+            cx.moveTo(x, y)
+
+            //drawfigure2Border(figure2)
+        }
+    };
+
+    function getpixelcolour(x, y) {
+        let index = ((y * (pixels.width * 4)) + (x * 4));
+        return {
+            r: pixels.data[index],
+            g: pixels.data[index + 1],
+            b: pixels.data[index + 2],
+            a: pixels.data[index + 3]
+        };
+    }
+
+    function getpixelamount(r, g, b) {
+        let pixels = cx.getImageData(0, 0, c.width, c.height);
+        let all = pixels.data.length;
+        let amount = 0;
+        for (i = 0; i < all; i += 4) {
+            if (pixels.data[i] === r &&
+                pixels.data[i + 1] === g &&
+                pixels.data[i + 2] === b) {
+                amount++;
+            }
+        }
+        return amount;
+    };
+
+    function pixelthreshold() {
+
+        if (getpixelamount(r, g, b) / figure2pixels > 1.2) {
+            if (!isfigure2Complete) {
+                pulse()
+                
+            }
+        }
+    };
+
+    function pulse() {
+       let size = 150
+        let timerId1 = setInterval(() => {
+            size += 1
+            cx.clearRect(0, 0, c.width, c.height)
+            drawfigure2(size,`rgb(${r}, ${g}, ${b + 1})`)
+        }, 40);
+
+        setTimeout(() => {
+            clearInterval(timerId1);
+            let timerId2 = setInterval(() => {
+                size -= 1
+                cx.clearRect(0, 0, c.width, c.height)
+                
+                drawfigure2(size, `rgb(${r}, ${g}, ${b + 1})`)
+
+            }, 40);
+            setTimeout(() => {
+                clearInterval(timerId2);
+            }, 800);
+        }, 800);
+
+        isfigure2Complete = true
+    }
+
+    function onmousedown(ev) {
+        mousedown = true;
+        ev.preventDefault();
+
+    };
+
+    function onmouseup(ev) {
+        mousedown = false;
+        pixelthreshold();
+        ev.preventDefault();
+        cx.beginPath();
+    };
+
+    function onmousemove(ev) {
+        if (mousedown) {
+            //var area = ev.width * ev.height;
+            let x = Math.round(ev.clientX - c.getBoundingClientRect().x);
+            let y = Math.round(ev.clientY - c.getBoundingClientRect().y);
+            paint(x, y);
+        }
+    };
+
+    function reloadTask() {
+        isfigure2Complete = false
+        setupCanvas();
+    }
+
+    drop.addEventListener('click', reloadTask)
+
+    c.addEventListener('pointerdown', onmousedown, false);
+    c.addEventListener('pointerup', onmouseup, false);
+    c.addEventListener('pointermove', onmousemove, false);
+
+
+    setupCanvas();
+
+
+
+})()
---
+++ js-data/figure4Tracing.js
@@ -0,0 +1,224 @@
+(() => {
+
+    /*все вхождения figure2_A... заменить A на нужную букву*/
+    let task = document.querySelector('.figure4_wrapper')
+    const interakt_zadanie = task.parentElement;
+    const headCheck = interakt_zadanie.previousElementSibling;
+    const drop = headCheck.querySelector('.drop');
+    const figure2Picture = task.querySelector('.figure2_picture')
+
+    let c = task.querySelector('.figure2_A_canvas');
+    let cx = c.getContext('2d');
+    let mousedown = false;
+
+    let pixels = null;
+    let figure2pixels = null;
+    /*заменить Aa на нужные буквы*/
+    //let figure2 = '◻'// △ ◯ ▭ ◻ ◇
+    /**/
+    /*при необходимости подобрать нужный шрифт*/
+    //let font = 'Titillium Web'
+    //cx.font = `bold 310px ${font}`;
+    /**/
+    let isfigure2Complete = false
+    /*заменть цвет закраски буквы на нужный в формате rgb*/
+    /*использовать любой кроме 
+    (255,255,255)-цвет буквы и
+    (240, 100, 0)-цвет обводки
+    (значение 255 нельзя применять ни к одому из трех каналов цвета)*/
+    let r = 220
+    let g = 0
+    let b = 0
+    /**/
+    let drowColor = `rgb(${r}, ${g}, ${b})`
+
+    function setupCanvas() {
+        c.height = 400;
+        c.width = 500;
+        cx.lineCap = 'round';
+        // cx.font = `bold 500px ${font}`;
+        //cx.fillStyle = 'rgb(255, 255, 255)';
+        //cx.textBaseline = 'middle';
+
+        drawfigure2(0, 'rgb(230,230,230)');
+        
+        pixels = cx.getImageData(0, 0, c.width, c.height);
+        figure2pixels = getpixelamount(230, 230, 230);
+    }
+
+    function drawfigure2(size, color) {
+        
+        //cx.fillStyle =`rgb(${r}, ${g}, ${b+1})`
+        
+        drawTriangle(50-size, 250+size, 300+size, `rgb(${r}, ${g}, ${b+1})`)
+        drawTriangle(75-size, 210+size, 250+size, color )
+        drawTriangle(110-size, 160+size, 190+size, `rgb(${r}, ${g}, ${b+1})` )
+        drawTriangle(140-size, 115+size, 135+size, 'white' )
+    };
+
+    function drawTriangle(top_padding, height, width,color) {
+        let centerX = c.width / 2;
+        let centerY = c.height / 2;
+        cx.moveTo(centerX- width/2, 0 + height + top_padding);
+        cx.beginPath()
+        cx.lineTo(centerX, 0 + top_padding);
+        cx.lineTo(centerX+ width/2, 0 + height + top_padding);
+        cx.lineTo(centerX- width/2, 0 + height + top_padding);
+        cx.fillStyle =color
+        cx.fill()
+    }
+
+    function strokeStar( r, n, inset, color) {
+        let centerX = c.width / 2;
+        let centerY = c.height / 2;
+        cx.save();
+        cx.beginPath();
+        cx.translate(centerX, centerY);
+        cx.moveTo(0,0-r);
+        for (var i = 0; i < n; i++) {
+            cx.rotate(Math.PI / n);
+            cx.lineTo(0, 0 - (r*inset));
+            cx.rotate(Math.PI / n);
+            cx.lineTo(0, 0 - r);
+        }
+        cx.closePath();
+        cx.fillStyle = color;
+        cx.fill();
+        cx.restore();
+    }
+
+    function drawfigure2Border(figure2) {
+        let centerx = (c.width - cx.measureText(figure2).width) / 2;
+        let centery = c.height / 2;
+        cx.lineWidth = 5;
+        cx.lineCap = 'round'
+        cx.strokeStyle = 'rgb(240, 100, 0)';
+        cx.strokeText(figure2, centerx, centery);
+    };
+
+    function showerror(error) {
+        mousedown = false;
+        alert(error);
+    };
+
+    function paint(x, y) {
+        let colour = getpixelcolour(x, y);
+
+        if (colour.r !== 230 || colour.g !== 230 || colour.b !== 230) {
+
+            mousedown = false;
+        } else {
+            cx.strokeStyle = drowColor;
+
+            cx.lineWidth = 24;
+            cx.beginPath();
+            cx.lineTo(x, y);
+            cx.stroke();
+            cx.beginPath();
+            cx.stroke();
+
+            cx.beginPath();
+            cx.moveTo(x, y)
+
+            //drawfigure2Border(figure2)
+        }
+    };
+
+    function getpixelcolour(x, y) {
+        let index = ((y * (pixels.width * 4)) + (x * 4));
+        return {
+            r: pixels.data[index],
+            g: pixels.data[index + 1],
+            b: pixels.data[index + 2],
+            a: pixels.data[index + 3]
+        };
+    }
+
+    function getpixelamount(r, g, b) {
+        let pixels = cx.getImageData(0, 0, c.width, c.height);
+        let all = pixels.data.length;
+        let amount = 0;
+        for (i = 0; i < all; i += 4) {
+            if (pixels.data[i] === r &&
+                pixels.data[i + 1] === g &&
+                pixels.data[i + 2] === b) {
+                amount++;
+            }
+        }
+        return amount;
+    };
+
+    function pixelthreshold() {
+
+        if (getpixelamount(r, g, b) / figure2pixels > 1.4) {
+            if (!isfigure2Complete) {
+                pulse()
+                
+            }
+        }
+    };
+
+    function pulse() {
+       let size = 0
+        let timerId1 = setInterval(() => {
+            size += 1
+            cx.clearRect(0, 0, c.width, c.height)
+            drawfigure2(size,`rgb(${r}, ${g}, ${b + 1})`)
+        }, 40);
+
+        setTimeout(() => {
+            clearInterval(timerId1);
+            let timerId2 = setInterval(() => {
+                size -= 1
+                cx.clearRect(0, 0, c.width, c.height)
+                
+                drawfigure2(size, `rgb(${r}, ${g}, ${b + 1})`)
+
+            }, 40);
+            setTimeout(() => {
+                clearInterval(timerId2);
+            }, 800);
+        }, 800);
+
+        isfigure2Complete = true
+    }
+
+    function onmousedown(ev) {
+        mousedown = true;
+        ev.preventDefault();
+
+    };
+
+    function onmouseup(ev) {
+        mousedown = false;
+        pixelthreshold();
+        ev.preventDefault();
+        cx.beginPath();
+    };
+
+    function onmousemove(ev) {
+        if (mousedown) {
+            //var area = ev.width * ev.height;
+            let x = Math.round(ev.clientX - c.getBoundingClientRect().x);
+            let y = Math.round(ev.clientY - c.getBoundingClientRect().y);
+            paint(x, y);
+        }
+    };
+
+    function reloadTask() {
+        isfigure2Complete = false
+        setupCanvas();
+    }
+
+    drop.addEventListener('click', reloadTask)
+
+    c.addEventListener('pointerdown', onmousedown, false);
+    c.addEventListener('pointerup', onmouseup, false);
+    c.addEventListener('pointermove', onmousemove, false);
+
+
+    setupCanvas();
+
+
+
+})()
